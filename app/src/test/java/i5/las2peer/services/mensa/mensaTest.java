edde86ea0eedd0344a4c601e383ddd52c8dcb820
GHProjectsUser <mail@rwth>
2023-07-12 14:00:56 +0000

    Add tests

--- app/src/test/java/i5/las2peer/services/mensa/mensaTest.java
+++ app/src/test/java/i5/las2peer/services/mensa/mensaTest.java
@@ -116,6 +116,65 @@ public class mensaTest {
   }
 
 
+  /**
+   * 
+   * Test for the Listavailableratingsforadish_ID383740 method.
+   * 
+   */
+  @Test
+  public void testListavailableratingsforadish_ID383740() {
+    MiniClientCoverage c = new MiniClientCoverage(mainPath);
+    c.setConnectorEndpoint(connector.getHttpEndpoint());
+    
+        
+    try {
+      c.setLogin(AnonymousAgentImpl.IDENTIFIER, "");
+      ClientResponse result = c.sendRequest("GET", "/dishes/{id}/ratings", """
+""", "text/plain", "*/*", new HashMap<>(), "1");
+      System.out.println("Result of request with id: 397957: " + result.getResponse().trim());
+      Object response = JSONValue.parse(result.getResponse().trim());
+      // Response body all list entries has field "stars" has type Number
+      assertThat("[580251]", response, both(isA(JSONArray.class)).and(asJSONObjectList(everyItem(both(isA(JSONObject.class)).and(asJSONObject(hasField("stars", isA(Number.class))))))));
+      
+
+      Assert.assertEquals("[327492]", 200, result.getHttpCode());
+      // Response body has type JSON Array
+      assertThat("[775686]", response, isA(JSONArray.class));
+      
+
+    } catch (Exception e) {
+      e.printStackTrace();
+      fail("Exception: " + e);
+    }
+
+    
+  }
+  /**
+   * 
+   * Test for the GETdishesidratingsnotfoundtest_ID637979 method.
+   * 
+   */
+  @Test
+  public void testGETdishesidratingsnotfoundtest_ID637979() {
+    MiniClientCoverage c = new MiniClientCoverage(mainPath);
+    c.setConnectorEndpoint(connector.getHttpEndpoint());
+    
+        
+    try {
+      c.setLogin(AnonymousAgentImpl.IDENTIFIER, "");
+      ClientResponse result = c.sendRequest("GET", "/dishes/{id}/ratings", """
+""", "text/plain", "*/*", new HashMap<>(), "99999");
+      System.out.println("Result of request with id: 81526: " + result.getResponse().trim());
+    
+      Assert.assertEquals("[87949]", 404, result.getHttpCode());
+
+    } catch (Exception e) {
+      e.printStackTrace();
+      fail("Exception: " + e);
+    }
+
+    
+  }
 
 
 
